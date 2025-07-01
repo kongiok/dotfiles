9428086dc21fdc9c70f850bb8ed386bfb843c4f4
kongiok <ong.kongiok@gmail.com>
2025-07-01 08:26:48 +0800

    fix(scripts): recreate install script
    I accidentally delete the brew install script in previous commit. So I decided
    to rewrite it and fix the path bug that could not detect brew files.

---
+++ home/.chezmoiscripts/run_once_brew_install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [[ "$(uname)" -ne "Darwin" ]]; then
+  printf "brew pkgs | this script is only for macOS" >&2
+  exit 0
+fi
+
+printf "brew pkgs | installing packages\n"
+
+if ! command -v brew >/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [[ ! $? -eq 0 ]]; then
+    printf "brew pkgs | unabled to install brew package manager.\nTry install it on your own and run this script again." >&2
+  fi
+fi
+
+if ! command -v brew >/dev/null 2>&1; then
+  printf "brew pkgs | there's no brew command right there" >&2
+fi
+
+brewfile="$HOME/.config/pkgs/osx/Brewfile"
+
+if [[ ! -f $brewfile ]]; then
+  printf "brew pkgs | there's no brew files\n" >&2
+  exit 1
+fi
+
+brew bundle --file $brewfile
+
+if [[ ! $? -eq 0 ]]; then
+  printf "brew pkgs | unabled to install pkgs by using $(brew bundle)" >&2
+fi
