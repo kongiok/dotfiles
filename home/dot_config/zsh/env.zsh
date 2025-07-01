bb40cd48be875798238b74bd0d198fe959fbde80
kongiok <ong.kongiok@gmail.com>
2025-07-01 23:16:53 +0800

    feat(lang): add language manager mise

--- home/dot_config/zsh/env.zsh
+++ home/dot_config/zsh/env.zsh
@@ -1,2 +1,9 @@
 export PAGER="nvimpager"
 export HOMEBREW_NO_ENV_HINTS=true
+
+if command -v brew >/dev/null 2>&1; then
+	eval "$(brew shellenv)"
+fi
+if command -v mise >/dev/null 2>&1; then
+	eval "$(mise activate zsh)"
+fi
