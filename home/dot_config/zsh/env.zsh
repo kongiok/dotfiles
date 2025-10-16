26defc197a670e8af484efc905ff34818d07b5e0
kongiok <ong.kongiok@gmail.com>
2025-10-16 21:44:16 +0800

    feat(password): add ssh key agent storage via bitwarden

--- home/dot_config/zsh/env.zsh
+++
@@ -1,9 +0,0 @@
-export PAGER="nvimpager"
-export HOMEBREW_NO_ENV_HINTS=true
-
-if command -v brew >/dev/null 2>&1; then
-	eval "$(brew shellenv)"
-fi
-if command -v mise >/dev/null 2>&1; then
-	eval "$(mise activate zsh)"
-fi
