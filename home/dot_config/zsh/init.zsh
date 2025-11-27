dd30c4d5e51b7b541c6777226b97ec291106ed72
kongiok <ong.kongiok@proton.me>
2025-11-28 02:32:33 +0800

    chore(Shell): add terminal specific configs

--- home/dot_config/zsh/init.zsh
+++ home/dot_config/zsh/init.zsh
@@ -3,6 +3,11 @@
 # plugins loaded with sheldon
 eval "$(sheldon source)"
 
+# Customization for interactive shell
+if [ "${ZED_TERM}" ] && [[ "${ZED_TERM}" == "true" ]] ;then
+	return
+fi
+
 if [[ -z "$ZELLIJ" ]] && command -v zellij &>/dev/null; then
   exec zellij
 fi
