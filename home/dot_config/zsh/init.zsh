6bf986f6d5d4236551276083a956d610f2d480b1
kongiok <ong.kongiok@proton.me>
2026-01-17 10:00:48 +0800

    feat(linux): add zellij plugin for terminal based on ssh connection or default terminals

--- home/dot_config/zsh/init.zsh
+++ home/dot_config/zsh/init.zsh
@@ -3,23 +3,32 @@
 # plugins loaded with sheldon
 eval "$(sheldon source)"
 
-# Specify whether to use Zellij or not
 typeset -A should_open_zellij=(
-  [default]       false
+  DEFAULT         false
   Apple_Terminal  true
   iTerm.app       true
   WezTerm         true
   ZED_TERM        false
   TTY             true
+  SSH             true
 )
 
-if [ -t 1 ]; then
-  TERM_PROGRAM="TTY"
+local current_env="$TERM_PROGRAM"
+
+if [[ -n "$SSH_CONNECTION" || -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
+  current_env="SSH"
+elif [[ -z "$current_env" ]]; then
+  if [[ -t 1 ]]; then # check whether is TTY
+    current_env="TTY"
+  else
+    current_env="DEFAULT"
+  fi
 fi
 
-
-if [ "${should_open_zellij[${TERM_PROGRAM}]:-${should_open_zellij[\[default\]]}}" = true ]; then
+if [[ "${should_open_zellij[$current_env]:-${should_open_zellij[DEFAULT]}}" == "true" ]]; then
   if [[ -z "$ZELLIJ" ]] && command -v zellij &>/dev/null; then
-    exec zellij
+    if [[ -o interactive ]]; then
+      exec zellij
+    fi
   fi
 fi
