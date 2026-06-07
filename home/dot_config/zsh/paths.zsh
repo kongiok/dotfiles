82c2ac923a6faa37b292f46fafce9efd72c4a871
kongiok <ong.kongiok@proton.me>
2026-06-07 17:36:06 +0800

    fix(path): path does not matches

--- home/dot_config/zsh/paths.zsh
+++ home/dot_config/zsh/paths.zsh
@@ -2,7 +2,7 @@ path=(
   "$HOME/.cargo/bin" # Cargo bin
   "$HOME/.local/bin" # Local bin (XDG Standard)
   "$HOME/.lmstudio/bin" # LMStudio bin
-  "$HOME/.bun/bin/"	# JavaScript Backend,
+  "$HOME/.bun/bin"	# JavaScript Backend,
   "$HOME/.config/broot/launcher/bash/br" # Broot cmd
   $path
 )
